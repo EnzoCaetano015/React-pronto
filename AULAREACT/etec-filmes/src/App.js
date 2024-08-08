a64d1cf21e107dd401914afcd440a37eac85e79a
EnzoCaetano015 <peracioenzo@gmail.com>
2024-08-08 11:00:42 -0300

    aula 08/08

--- AULAREACT/etec-filmes/src/App.js
+++ AULAREACT/etec-filmes/src/App.js
@@ -1,11 +1,18 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <body>
       <Header></Header>
-      <h1>ETEC MCM</h1>
-      <p>Olá Mundo</p>
+      <Banner></Banner>
+      <Container>
+        <h1>ETEC MCM</h1>
+        <p>Olá Mundo</p>
+      </Container>
+      <Footer></Footer>
     </body>
   );
 }
